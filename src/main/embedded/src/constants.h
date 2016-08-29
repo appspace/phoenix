46d85c2055d7e567569f20dbe7ae5d783564f21d
Eugene Sukharev <eugenes@ecobee.com>
2016-08-28 20:50:42 -0400

    Header file for embedded

---
+++ src/main/embedded/src/constants.h
@@ -0,0 +1,5 @@
+#define SSID_NAME "Your wifi name here";
+#define SSID_PASSWORD "Your wifi password here";
+#define API_GATEWAY_URL "https://lzwo7r0mek.execute-api.us-east-1.amazonaws.com/prod/adc";
+#define API_GATEWAY_CLIENT_KEY "Client ID from your API gateway";
+#define API_GATEWAY_SSL_FINGERPRINT "9E 22 1B FA FA F9 70 05 CF CE 1D CD D1 9A E9 01 1D 0B 7A 82";  //This if fingerprint of amazonaws.com SSL key
